d50078ba77f991c9ad53d9c9aaef0c256aa25ed9
itdlatte <itd@ticini.com>
2022-01-18 01:45:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -616,7 +616,7 @@ def admin_organizer_detail(id):
     else:
         userVotings = None
     cur.close()
-    now = datetime.now()
+    now = datetime.now(IST)
     voting = count_voting_detail(id)
     return render_template('adminOrganizerDetail.html', core = core, userDetail = userDetail, userVotings = userVotings, voting = voting, now = now)
 
